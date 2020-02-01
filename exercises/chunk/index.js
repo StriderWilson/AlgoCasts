eaa47d675a94a9b915160116665b2bee312fb15e
Strider Wilson <Strider@Striders-MacBook-Pro.local>
2020-01-31 19:21:33 -0800

    Solve chunk by brute force

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,24 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    var result = [];
+    var chunk = [];
+    if (array.length < size) {
+        result.push(array);
+    } else {
+        for (var i = 0; i < array.length; i++) {
+            chunk.push(array[i]);
+            if (chunk.length === size) {
+                result.push(chunk);
+                chunk = [];
+            }
+        }
+    }
+    if (chunk.length > 0) {
+        result.push(chunk);
+    }
+    return result;
+}
 
 module.exports = chunk;
