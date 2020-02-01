fd172bd2de4fb2671124444efe0e102f6409bac9
Strider Wilson <Strider@Striders-MacBook-Pro.local>
2020-01-31 19:28:30 -0800

    Solve chunk using slice

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -10,22 +10,33 @@
 
 function chunk(array, size) {
     var result = [];
-    var chunk = [];
-    if (array.length < size) {
-        result.push(array);
-    } else {
-        for (var i = 0; i < array.length; i++) {
-            chunk.push(array[i]);
-            if (chunk.length === size) {
-                result.push(chunk);
-                chunk = [];
-            }
-        }
-    }
-    if (chunk.length > 0) {
-        result.push(chunk);
+    var index = 0;
+
+    while (index < array.length) {
+        result.push(array.slice(index, index + size));
+        index += size;
     }
     return result;
 }
+
+// function chunk(array, size) {
+//     var result = [];
+//     var chunk = [];
+//     if (array.length < size) {
+//         result.push(array);
+//     } else {
+//         for (var i = 0; i < array.length; i++) {
+//             chunk.push(array[i]);
+//             if (chunk.length === size) {
+//                 result.push(chunk);
+//                 chunk = [];
+//             }
+//         }
+//     }
+//     if (chunk.length > 0) {
+//         result.push(chunk);
+//     }
+//     return result;
+// }
 
 module.exports = chunk;
