1ced751a41b09ee6db1ff39ad85fe0ec33a5a882
Strider Wilson <Strider@Striders-MacBook-Pro.local>
2020-02-05 11:26:14 -0800

    Solve anagrams using objects

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,26 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    stringA = stringA.replace(/[^\w]/g, "").toLowerCase();
+    stringB = stringB.replace(/[^\w]/g, "").toLowerCase();
+    if (stringA.length !== stringB.length) {
+        return false;
+    }
+    var objA = {};
+    var objB = {};
+    for (char of stringA) {
+         objA[char] = objA[char] + 1 || 1;
+    }
+    for (char of stringB) {
+        objB[char] = objB[char] + 1 || 1;
+    }
+    for (let char in objA) {
+        if (objA[char] !== objB[char]) {
+           return false;
+        }
+    }
+    return true;
+}
 
 module.exports = anagrams;
